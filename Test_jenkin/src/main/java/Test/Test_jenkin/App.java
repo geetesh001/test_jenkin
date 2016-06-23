f9f683926ed3a69263a7af0a3fda3d96d2c5fe9e
papi <papi@geetesh>
2016-06-23 08:52:08 +0530

    1st commit

---
+++ Test_jenkin/src/main/java/Test/Test_jenkin/App.java
@@ -0,0 +1,13 @@
+package Test.Test_jenkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Test_jenkin/src/test/java/generic/Testing.java
@@ -0,0 +1,12 @@
+package generic;
+
+import org.testng.annotations.Test;
+
+public class Testing {
+	
+	@Test
+	public void display(){
+		System.out.println("Hello world");
+	}
+
+}
