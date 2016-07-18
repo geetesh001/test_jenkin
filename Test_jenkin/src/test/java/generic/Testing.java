3cfbb9064859bc25a1f178e507789f5d49318624
papi <papi@geetesh>
2016-07-18 20:20:32 +0530

    Adding selenium function

--- Test_jenkin/src/test/java/generic/Testing.java
+++ Test_jenkin/src/test/java/generic/Testing.java
@@ -1,5 +1,9 @@
 package generic;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Testing {
@@ -7,6 +11,16 @@ public class Testing {
 	@Test
 	public void display(){
 		System.out.println("Hello world");
+	}
+	
+	@Test
+	public void first()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\papi\\git\\test_jenkin\\Test_jenkin\\src\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.facebook.com");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 }
